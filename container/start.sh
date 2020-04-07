ea77b40c813d9af1be5b48d0e5eeaf1e74753473
Christophe Tanguay <cktanguay@gmail.com>
2020-04-06 22:04:19 -0400

    update fichier docker compose et start

--- container/start.sh
+++ container/start.sh
@@ -1,18 +1,9 @@
 #!/bin/sh
 sudo docker-compose down
-sudo docker run \
-  --volume=/:/rootfs:ro \
-  --volume=/var/run:/var/run:ro \
-  --volume=/sys:/sys:ro \
-  --volume=/var/lib/docker/:/var/lib/docker:ro \
-  --volume=/dev/disk/:/dev/disk:ro \
-  --publish=8080:8080 \
-  --detach=true \
-  --name=cadvisor \
-  gcr.io/google-containers/cadvisor:v0.35.0
+
+
+sudo docker stop $(sudo docker ps -aq)
+sudo docker rm $(sudo docker ps -aq)
 
 sudo sysctl -w vm.max_map_count=262144
-
-sudo docker run --name mynginx1 -p 80:80 -d nginx
-
-sudo docker-compose up --scale es01=3 --remove-orphans
+sudo docker-compose up --scale es01=2 --remove-orphans
